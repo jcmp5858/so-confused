8fee8feb674a1d2f0368ddbf877b1e636435de2b
Julia P <95725122+jcmp5858@users.noreply.github.com>
2022-08-02 14:13:33 -0400

    Update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -6,3 +6,14 @@ exports.default = () => (
 		.pipe(imagemin())
 		.pipe(gulp.dest('./public/images'))
 );
+
+let numOfSeats = 28;
+//number of seats in my classroom
+let Class1 = 22; 
+//English Block 001 
+let Class2 = 22; 
+//English Block 004
+let Class3 = 28;
+//English Block Honors 
+let groupings = Class1 / 4;
+console.log(groupings);
